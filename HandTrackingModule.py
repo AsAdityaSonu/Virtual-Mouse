31a25f5e3aec4ddd2b033c43cc48739c5149d941
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2023-11-07 04:16:48 +0530

    Hand Detector function added

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -4,20 +4,43 @@ import time
 import math
 import numpy as np
 
+class HandDetector:
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, frame, draw=True):
+        frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(frameRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(frame, handLms, self.mpHands.HAND_CONNECTIONS)
+        return frame
 
 def main():
     pTime = 0
     cTime = 0
     cap = cv2.VideoCapture(0)
+    detector = HandDetector()   # Object
 
     while True:
         ret, frame = cap.read()
 
+        frame = detector.findHands(frame)
+        
         # FPS on screen
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
-        cv2.putText(frame, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3)
+        cv2.putText(frame, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 2)
 
         # Displaying Video
         cv2.imshow("frame", frame)
@@ -26,7 +49,6 @@ def main():
 
     cap.release()  # Release the video capture object
     cv2.destroyAllWindows()  # Close all OpenCV windows
-
 
 if __name__ == "__main__":
     main()
